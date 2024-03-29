de6094d13cbfac7ec26c1494e605c96d66c4a58c
ayush41 <bhattayushraj@gmail.com>
2024-03-30 03:02:10 +0530

    feature

---
+++ script.js
@@ -0,0 +1,11 @@
+let checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
